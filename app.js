5fce2fe5b0922b681a76f65f74a6ee827d178e8b
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-04-08 12:38:37 +0530

    starting work on dash

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.get('/placeOrder',function(err,res){
 app.get('/getAssetDetails',function(err,res){
     res.render('assetDetails.html');
 });
+app.get('/rawMaterial',function(err,res){
+    res.render('rawMaterial.html');
+});
+app.get('/production',function(err,res){
+    res.render('production.html');
+});
+app.get('/shipment',function(err,res){
+    res.render('shipping.html');
+});
+app.get('/logistic',function(err,res){
+    res.render('logistic.html');
+});
+
 app.listen(3000,function () {
     console.log("Server started on port 3000");
 });
