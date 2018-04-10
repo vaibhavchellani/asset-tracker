38b6c16844d61b1b92f7307e02cf89250b19dadc
Vaibhav Chellani <vaibhavchellani223@gmail.com>
2018-04-11 00:29:33 +0530

    done all

--- app.js
+++ app.js
@@ -25,6 +25,18 @@ app.get('/logistic',function(err,res){
 app.get('/rawMaterialDash',function(err,res){
     res.render('rawMaterialDash.html');
 });
+app.get('/productionDash',function(err,res){
+    res.render('productionDash.html');
+});
+app.get('/shippingDash',function(err,res){
+    res.render('shippingDash.html');
+});
+app.get('/logisticDash',function(err,res){
+    res.render('logisticDash.html');
+});
+app.get('/readyDash',function(err,res){
+    res.render('readyDash.html');
+});
 app.listen(3000,function () {
     console.log("Server started on port 3000");
 });
